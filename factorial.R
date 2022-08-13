943f7031d79d71a3300b275ab89ecd4abf14b052
Francisco Pérez Mogollón <59151009+franperezec@users.noreply.github.com>
2022-08-12 21:08:18 -0500

    Add files via upload
    Factorial function to R.

---
+++ factorial.R
@@ -0,0 +1,14 @@
+factorial <- function(n){
+  factorial <-1
+  if(n < 0){
+    print("No es posible calcular el factorial de un numero negativo")
+  }else if(n == 0){
+    print("Su factorial es 0")
+  }else{
+    for (i in 1:n) {
+      factorial<-factorial*i
+    }
+    print(paste("El factorial de ", n))
+    print(paste("es: ", factorial))
+  } 
+}
